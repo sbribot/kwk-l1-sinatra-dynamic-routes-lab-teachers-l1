b2540946f13f81bdca903c216e18c37849a1cf2d
Sara Beth Ribot <sbribot@gmail.com>
2018-06-26 21:56:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,13 +5,14 @@ class App < Sinatra::Base
   # Write your code here!
 
 get '/say/:number/:phrase' do
-#   @number = params[:number].to_i
-#   @phrase = params[:phrase]
-# times = 0 
-# until @number == times do
-#   "#{@phrase}"
-#   times += 1
-# end 
+  number = params[:number].to_i
+  phrase = params[:phrase]
+
+times = 0 
+until @number == times do
+  "#{@phrase}"
+  times += 1
+end 
 "#{params}"
 end
 
