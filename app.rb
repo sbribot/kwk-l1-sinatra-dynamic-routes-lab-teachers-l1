95e78c8962b73e0ed5a4de5412c88b420d4f549c
Sara Beth Ribot <sbribot@gmail.com>
2018-06-26 21:16:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+get '/say/:number/:phrase' do
+  times = 0 
+  until params[:number] >= times do 
+    params[:phrase]
+  end 
 end
